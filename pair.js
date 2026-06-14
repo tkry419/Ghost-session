3bf8737c8dc71660b4a364193fa5c5f424d07388
tkry419 <chukwudiasika16@gmail.com>
2026-06-14 11:15:38 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -28,8 +28,8 @@ function randomMegaId(length = 6, numberLength = 4) {
 async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
-            email: 'nexusxd.bot@gmail.com', // Your Mega A/c Email Here
-            password: 'malvin266' // Your Mega A/c Password Here
+            email: 'bosstkry@gmail.com', // Your Mega A/c Email Here
+            password: 'chukwudi419' // Your Mega A/c Password Here
         }).ready;
         console.log('Mega storage initialized.');
 
